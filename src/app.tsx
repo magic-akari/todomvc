3d0fc4bdcaf1a4a70a52c91a7e852d78a5e1de28
阿卡琳 <hufan.akarin@Gmail.com>
2018-04-13 13:24:15 +0800

    optimize

--- src/app.tsx
+++ src/app.tsx
@@ -6,9 +6,7 @@ declare const ReactDOM: typeof _ReactDOM;
 import { TodoModel } from "./models/todoModel.js";
 import { TodoApp } from "./components/App.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-	ReactDOM.render(
-		<TodoApp model={new TodoModel("typescript-react-esm-todos")} />,
-		document.querySelector(".todoapp")
-	);
-});
+ReactDOM.render(
+	<TodoApp model={new TodoModel("typescript-react-esm-todos")} />,
+	document.querySelector(".todoapp")
+);
