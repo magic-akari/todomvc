68bf548af7d28f49084b0e0ac2661ee0fc7ea865
magic-akari (via Travis CI) <akari.ccino@gmail.com>
2021-04-24 14:06:03 +0000

    Deploy magic-akari/todomvc to github.com/magic-akari/todomvc.git:gh-pages

--- components/App.js
+++ components/App.js
@@ -42,7 +42,7 @@ export class TodoApp extends React.Component {
         this.clearCompleted = () => {
             this.props.model.clearCompleted();
         };
-        this.props.model.subscribe(this.setState.bind(this));
+        this.props.model.subscribe(this.setState.bind(this, this.state));
     }
     componentDidMount() {
         window.addEventListener("hashchange", () => {
